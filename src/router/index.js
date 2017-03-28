94d5479a689390523f4c41d9c7e21a0633bb2939
weizidong <weizidong@ichuangye.cn>
2017-03-28 15:59:32 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import routes from "./routers"
-
 Vue.use(Router)
 const router = new Router({
   mode: 'history',
   routes
 })
-
 export default router
