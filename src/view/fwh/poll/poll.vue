f0ec87419ff297af6f4ac9afb604265ce3cf8961
zhongcheng <zhongcheng@ichuangye.cn>
2017-04-13 22:13:03 +0800

    update

---
+++ src/view/fwh/poll/poll.vue
@@ -0,0 +1,93 @@
+<style lang="less" scoped>
+    @import "../../../../static/normal.less";
+    @import "./poll.less";
+</style>
+<template>
+    <div class="poll" style="background-color: #F0F0F0;height: 100%;overflow: scroll">
+        <div>
+            <div class="height:44px;">
+                <sticky :check-sticky-support="false">
+                    <tab :line-width=2 active-color='#fc378c' v-model="index">
+                        <tab-item class="vux-center" :selected="demo2 === item"
+                                  v-for="(item, index) in list2" @click="demo2 = item" :key="index">{{item}}</tab-item>
+                    </tab>
+                </sticky>
+            </div>
+            <div class="tab-box">
+                <ul v-if="index==0" class="askListUl">
+                    <li @click="goto(['pollquestion',{id:'zhongcheng'}])">
+                        <div class="askListHead">
+                            <div class="askListHeadBox1">
+                                <h3>1.问卷调查关于“2017龙泉驿区环境卫生”全面问卷调查普查关于“2017龙泉驿区环境卫生”全面问卷调查普查</h3>
+                                <p><span>积分奖励 : 50</span>  <a>已过期</a></p>
+                            </div>
+                            <div class="askListHeadBox2">
+                                要做一个在微信中打开的页面,需要获得微信导航头的高度,例子图要怎样才能取到那?或者是能取到除了头之外的
+                                屏幕宽高也可以...
+                            </div>
+                        </div>
+                        <div class="askListFoot">
+                            <span>调查时间：</span>2017-03-25 15:00  至  2017-03-30 15:00
+                        </div>
+                    </li>
+                </ul>
+                <ul v-if="index==1" class="askListUl">
+                    <li @click="goto(['pollvote',{id:'zhongcheng'}])">
+                        <div class="askListHead">
+                            <div class="askListHeadBox1">
+                                <h3>1.投票选举关于“2017龙泉驿区环境卫生”全面问卷调查普查关于“2017龙泉驿区环境卫生”全面问卷调查普查</h3>
+                                <p><span>积分奖励 : 50</span>  <a>已过期</a></p>
+                            </div>
+                            <div class="askListHeadBox2">
+                                要做一个在微信中打开的页面,需要获得微信导航头的高度,例子图要怎样才能取到那?或者是能取到除了头之外的
+                                屏幕宽高也可以...
+                            </div>
+                        </div>
+                        <div class="askListFoot">
+                            <span>调查时间：</span>2017-03-25 15:00  至  2017-03-30 15:00
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script type="es6">
+    import { mapGetters } from 'vuex'
+    import { mapActions } from 'vuex'
+    import {date3Filter} from '../../../filters'
+    import { Tab, TabItem, Sticky, Divider, XButton, Swiper, SwiperItem } from 'vux'
+
+    export default{
+        data(){
+            return{
+                list2: ['问卷调查', '投票选举'],
+                demo2: '美食',
+                index:0
+            }
+        },
+        components:{
+            Tab,
+            TabItem,
+            Sticky,
+            Divider,
+            XButton,
+            Swiper,
+            SwiperItem
+        },
+        computed: {...mapGetters([ 'page','list']),
+        },
+        methods:{
+            ...mapActions(['goto','clear','getHealth','clearPage']),
+            date3Filter,
+        },
+        created () {
+
+        },
+        destroyed(){
+            this.clear()
+        }
+    }
+</script>
---
+++ src/view/fwh/poll/pollquestion.vue
@@ -0,0 +1,227 @@
+<style lang="less">
+    @import "../../../../static/normal.less";
+    @import "./poll.less";
+    @import "../../../../static/swiper.min.css";
+</style>
+<template>
+    <div class="pollvote" style="background-color: #F0F0F0;height: 100%;overflow: scroll;padding-top:50px;">
+        <div class="centerMessHead">
+            <div flex>
+                <div  flex items="center" style="padding-left:10px;">
+                    <a href="javascript:history.go(-1);"> <img class="return" src="../../../../static/wx/return.png"> </a>
+                </div>
+                <div box="4" flex items="center" justify="center" class="headTitle">
+                    <h2>2017龙泉驿区环境投票选举啦啦啦啦选举啦啦啦啦选举啦啦啦啦</h2>
+                </div>
+                <div box="1" flex items="center">
+                    <a class="a" @click="openRules()">规则</a>
+                </div>
+            </div>
+        </div>
+        <ul class="askListUl" v-if="!isAnswer">
+            <li>
+                <div class="askListHead">
+                    <div class="askListHeadBox1">
+                        <h3>1.问卷调查关于“2017龙泉驿区环境卫生”全面问卷调查普查关于“2017龙泉驿区环境卫生”全面问卷调查普查</h3>
+                        <p><span>积分奖励 : 50</span>  <a>已过期</a></p>
+                    </div>
+                    <div class="askListHeadBox2">
+                        阅读优秀的作品：这是显而易见的，但立竿见影的方法。如果你不读更多的好作品，你就不知道如何写出更好的作品优秀的作家都是从阅读别人的佳作开始，接着开始模仿，最后超越他们，形成自己的风格。尽可能的多读名著，在看内容的时候，更要留意文章的问题和写作的技巧。尽可能多的写每天都写，如果可能话，每天写几次。你写得多了，也就写得好了。学习如何写作和其他的学问道理是一样的，熟能生巧。写写你自己，写写博客，向出版社投稿。只是写，全情投入的写，练得越多，你的写作水平就提升得越快。随时随地记下你的灵感：随身带一本小笔记本（纳博科夫身上装满了小卡片），当你对你构思的小说，文章，或是小说里的人物有什么灵感的时候，马上记下来。当你听别人谈话时的只言片语而所有顿悟时，或看到一段散文诗或是一句歌词让你很感动时，都可以马上当他们记下来。灵感总是转瞬即逝，你及时的记录下来，便可以成为你写作的素材。我的习惯是，为我的博客要写的文章列一个清单，不断的补充它。驿区总工会为响应政府对城市环境卫生的管控，龙泉驿区总工会为响应政府对城市环境卫生的管控，龙泉驿区总工会。
+                    </div>
+                </div>
+                <div class="askListFoot">
+                    <span>调查时间：</span>2017-03-25 15:00  至  2017-03-30 15:00
+                </div>
+            </li>
+        </ul>
+        <a @click="answer" class="baoming" v-if="!isAnswer">开始答题</a>
+        <div class="answerBox" v-show="isAnswer">
+            <p class="answerBoxTitle">共10题,当前第2题,答完问卷即可获得积分奖励</p>
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide" v-for="i in 20">
+                        <p class="answerTitle"> {{i}} 、龙泉驿区如果要打造一款缤纷7色额娱乐场地，您喜欢的颜色是什么？(多选)</p>
+                        <checklist :name="'demo'+i"   required :options="commonList"   @on-change="(v)=>change(i,v)">
+                        </checklist>
+                    </div>
+                </div>
+            </div>
+            <div class="answerBtn">
+                <a class="prev" @click="switchSwiper(1)"><img src="../../../../static/wx/prev.png"></a>
+                <a class="next" @click="switchSwiper(2)"><img src="../../../../static/wx/next.png"></a>
+            </div>
+        </div>
+
+
+        <div v-transfer-dom>
+            <x-dialog v-model="popupVisible" class="dialog-demo" :hideOnBlur="true">
+                <div class="popupBox">
+                    <div class="popupHead">
+                        规则详情
+                        <img src="../../../../static/wx/del.png" @click="popupVisible=false">
+                    </div>
+                    <div class="popupCont">
+                        <ul>
+                            <li flex>
+                                <span>名&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;称: </span>
+                                <div  box="5">2017龙泉优秀职工投票选举</div>
+                            </li>
+                            <li flex>
+                                <span>积分奖励: </span>
+                                <div box="5">50
+                                    <span>积分/天</span>
+                                </div>
+                            </li>
+                            <li flex>
+                                <span>答题次数: </span>
+                                <div box="5">5
+                                    <span>次/天/人</span>
+                                </div>
+                            </li>
+                            <li flex>
+                                <span>调查时间: </span>
+                                <div box="5">2017-4-20 10:20 至 2017-08-20 10:30</div>
+                            </li>
+                            <li flex>
+                                <span>答题规则:</span>
+                                <div box="5">规则规则规则规则规则规则规则规则规则规则规则规则规则规则规则规则规则规则
+                                    规则规则规则规则规则规则规则</div>
+                            </li>
+                        </ul>
+                        <!--<div class="rules" flex>-->
+                        <!--<span>规则:</span>-->
+                        <!--<div class="">-->
+                        <!--<p>1、规则11111111</p>-->
+                        <!--<p>2、规则11111111</p>-->
+                        <!--<p>3、规则11111111规则11111111规则11111111规则11111111规则11111111规则11111111规则11111111</p>-->
+                        <!--</div>-->
+                        <!--</div>-->
+                    </div>
+                </div>
+            </x-dialog>
+        </div>
+
+        <myImgDialog @on-result-change="onResultChange" :img="img" :title="title" :content="content" :btns="btns" :isShow="isshow"></myImgDialog>
+
+    </div>
+</template>
+
+<script type="es6">
+    import { mapGetters } from 'vuex'
+    import { mapActions } from 'vuex'
+    import {date3Filter} from '../../../filters'
+    import { XImg , TransferDom, Popup   ,Scroller,XDialog,Checklist} from 'vux'
+    import myImgDialog from '../../../components/public/img-dialog/imgDialog.vue'
+    import '../../../../static/swiper.js'
+    export default{
+        data(){
+            return{
+                list: [
+                    '../../../../static/wx/test/test1.jpg',
+                    '../../../../static/wx/test/test2.jpg',
+                    '../../../../static/wx/test/test3.jpg',
+                    '../../../../static/wx/test/test4.jpg',
+                    '../../../../static/wx/test/test5.jpg',
+                    '../../../../static/wx/test/test6.jpg',
+                    '../../../../static/wx/test/test7.jpg',
+                    '../../../../static/wx/test/test8.jpg',
+                ],
+                showImgDetail:false,
+                index:0,
+                popupVisible:false,
+                isshow:false,//控制弹窗
+                img:'../../../../static/wx/succ.png',//控制弹窗图片
+                title:'提示',   //控制弹窗标题
+                content:'恭喜您！报名成功',  //控制弹窗内容
+                btns: {btn:'确定'},
+                isAnswer:false,
+                swip:'',
+                commonList: [ 'A、男', 'B、女', 'C、秀吉' ],
+                errors:'',
+                saveQuestion:[['a'],['b'],['a','b']]
+            }
+        },
+        directives: {
+            TransferDom
+        },
+        components:{
+            XImg , Popup  , Scroller, XDialog, myImgDialog , Checklist
+        },
+        computed: {...mapGetters([ 'page']),
+        },
+        methods:{
+            ...mapActions(['goto','clear','getHealth','clearPage']),
+            date3Filter,
+            success (src, ele) {
+                console.log('success load', src);
+            },
+            error (src, ele, msg) {
+                console.log('error load', msg, src);
+            },
+            showImg(id){
+                this.showImgDetail = true;
+                this.$nextTick(() => {
+                    this.$refs.scrollerEvent.reset()
+                })
+            },
+            demo_onIndexChange(index){    //轮播
+                this.index=index;
+            },
+            openRules(){
+                this.popupVisible = true;
+            },
+            onResultChange(val){
+                this.isshow=val;//外层调用组件方注册变更方法，将组件内的数据变更，同步到组件外的数据状态中
+            },
+            isShow(val){
+                if (val==1){
+                    this.img='../../../../static/wx/succ.png';
+                    this.content='恭喜您！投票成功';
+                    this.btns={btn:'确定'};
+                }else if (val==2){
+                    this.img='../../../../static/wx/default.png';
+                    this.content='对不起，您已经投过票啦';
+                    this.btns={btn:'确定'};
+                }
+                this.isshow=true;
+            },
+            answer(){    //开始答题
+                this.isAnswer = true;
+                this.swip = new Swiper('.swiper-container', {
+                    paginationClickable: true,
+                    spaceBetween: 30,
+                    observer:true,//修改swiper自己或子元素时，自动初始化swiper
+                    observeParents:true//修改swiper的父元素时，自动初始化swiper
+                });
+                console.log(this.swip);
+            },
+            switchSwiper(index){
+                if (index==2){
+                    if (this.saveQuestion.length==0){
+                        alert('请至少选择一个答案');
+                    }else {
+                        this.swip.slideNext();
+                    }
+                }else {
+                    this.swip.slidePrev();
+
+                }
+            },
+            change (idx,val) {
+                // this.saveQuestion[idx]=val;
+                console.log(val)
+            }
+        },
+        mounted () {
+
+        },
+        created () {
+            // this.$nextTick(() => {
+            //     this.$refs.scrollerEvent.reset({top: 0});
+            // })
+        },
+        destroyed(){
+            this.clear()
+        }
+    }
+</script>
