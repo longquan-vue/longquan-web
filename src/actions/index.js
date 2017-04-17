3897e142d45c3d0ca387787f9552b578516bb7fc
weizidong <weizidong@ichuangye.cn>
2017-04-17 13:53:33 +0800

    修正提示

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,6 @@ export const info = (message = '删除成功！', opts = {duration: 2000, showCl
 export const error = (message = '删除成功！', opts = {duration: 2000, showClose: true}) => new Promise((resolve) => resolve(Message.error({message, ...opts})));
 
 export const msg = (title = '提示', message = '', type = 'success', opts = {}) => MessageBox({title, message, type, ...opts});
-export const alert = (title = '提示', message = '', type = 'success', opts = {}) => MessageBox.alert({title, message, type, ...opts});
-export const confirm = (title = '提示', message = '', type = 'success', opts = {}) => MessageBox.confirm({title, message, type, ...opts});
-export const prompt = (title = '提示', message = '', type = 'success', opts = {}) => MessageBox.prompt({title, message, type, ...opts});
+export const alert = (title = '提示', message = '', type = 'success', opts = {}) => MessageBox.alert(message, title, {type, ...opts});
+export const confirm = (title = '提示', message = '', type = 'success', opts = {}) => MessageBox.confirm(message, title, {type, ...opts});
+export const prompt = (title = '提示', message = '', type = 'success', opts = {}) => MessageBox.prompt(message, title, {type, ...opts});
