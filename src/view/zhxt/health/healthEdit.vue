356e04473705f218e479a8411bbed0e4a1a30322
weizidong <weizidong@ichuangye.cn>
2017-04-17 21:52:10 +0800

    健身中心修正

--- src/view/zhxt/health/healthEdit.vue
+++ src/view/zhxt/health/healthEdit.vue
@@ -9,80 +9,28 @@
     </div>
     <div class="contentBoxCont">
       <div style="width:80%;margin:auto;">
-        <el-form :model="data" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
-          <el-form-item label="活动配图">
+        <el-form :model="data" :rules="rules" ref="ruleForm" label-width="160px" class="demo-ruleForm">
+          <el-form-item label="项目图标：">
             <MyUpload :files="data.files" :edit="data.edit"/>
           </el-form-item>
-          <el-form-item label="活动名称" prop="name">
+          <el-form-item label="项目名称：" prop="name">
             <el-input :value="data.name" @input="(v)=>setData({name:v})"></el-input>
           </el-form-item>
-          <el-form-item label="活动时间" required>
-            <el-col :span="11">
-              <el-form-item prop="start">
-                <el-date-picker type="datetime" format="yyyy-MM-dd HH:mm" placeholder="选择开始日期" :value="data.start" @input="(v)=>setData({start:v&&v.getTime()})" style="width: 100%;"></el-date-picker>
-              </el-form-item>
-            </el-col>
-            <el-col class="line" :span="2" style="text-align:center;">-</el-col>
-            <el-col :span="11">
-              <el-form-item prop="end">
-                <el-date-picker type="datetime" placeholder="选择结束日期" :value="data.end" @input="(v)=>setData({end:v&&v.getTime()})" style="width: 100%;"></el-date-picker>
-              </el-form-item>
-            </el-col>
+          <el-form-item label="项目类型：" required>
+            <el-input :value="data.name" @input="(v)=>setData({name:v})"></el-input>
           </el-form-item>
-          <el-form-item label="报名时间" required>
-            <el-col :span="11">
-              <el-form-item prop="entryStart">
-                <el-date-picker type="datetime" placeholder="选择开始日期" :value="data.entryStart" @input="(v)=>setData({entryStart:v&&v.getTime()})" style="width: 100%;"></el-date-picker>
-              </el-form-item>
-            </el-col>
-            <el-col class="line" :span="2" style="text-align:center;">-</el-col>
-            <el-col :span="11">
-              <el-form-item prop="entryEnd">
-                <el-date-picker type="datetime" placeholder="选择结束日期" :value="data.entryEnd" @input="(v)=>setData({entryEnd:v&&v.getTime()})" style="width: 100%;"></el-date-picker>
-              </el-form-item>
-            </el-col>
+          <el-form-item label="多人项目人数上限：" required>
+            <el-input :value="data.name" @input="(v)=>setData({name:v})"></el-input>
           </el-form-item>
-          <el-form-item label="报名人数" prop="total">
+          <el-form-item label="预约报名所需积分：" prop="total">
             <el-input :value="data.total" @input="(v)=>setData({total:v})"></el-input>
           </el-form-item>
-          <el-form-item label="报名权限" prop="entry">
-            <el-radio-group :value="data.entry" @input="(v)=>setData({entry:v})">
-              <el-radio :label="0">所有用户可报名</el-radio>
-              <el-radio :label="1">认证用户可报名</el-radio>
-            </el-radio-group>
+          <el-form-item label="开启日期：" prop="entry">
+            <InlineCalendar></InlineCalendar>
           </el-form-item>
-          <el-form-item label="报名所需积分" prop="score">
+          <el-form-item label="时间区间：" prop="score">
             <el-input :value="data.score" @input="(v)=>setData({score:v})"></el-input>
           </el-form-item>
-          <el-form-item label="主办方" prop="sponsor">
-            <el-input :value="data.sponsor" @input="(v)=>setData({sponsor:v})"></el-input>
-          </el-form-item>
-          <el-form-item label="协办方" prop="coSponsor">
-            <el-input :value="data.coSponsor" @input="(v)=>setData({coSponsor:v})"></el-input>
-          </el-form-item>
-          <el-form-item label="承办方" prop="organizer">
-            <el-input :value="data.organizer" @input="(v)=>setData({organizer:v})"></el-input>
-          </el-form-item>
-          <el-form-item label="活动地点" prop="place">
-            <el-input :value="data.place" @input="(v)=>setData({place:v})"></el-input>
-          </el-form-item>
-          <el-form-item label="福利提供方链接" prop="website">
-            <el-input :value="data.website" @input="(v)=>setData({website:v})"></el-input>
-          </el-form-item>
-          <el-form-item label="活动内容" prop="detail">
-            <quill-editor ref="myTextEditor" :content="data.detail" @input="(v)=>setData({detail:v})" :config="{}"></quill-editor>
-          </el-form-item>
-          <el-form-item label="活动规则介绍" prop="rule">
-            <quill-editor ref="myTextEditor" :content="data.rule" @input="(v)=>setData({rule:v})" :config="{}"></quill-editor>
-          </el-form-item>
-          <div v-if="!data.edit">
-            <el-form-item label="福利发布者" prop="admin">
-              <el-input v-model="data.admin.name" readonly></el-input>
-            </el-form-item>
-            <el-form-item label="发布时间" prop="created">
-              <el-input :value="date3Filter(data.created)" readonly></el-input>
-            </el-form-item>
-          </div>
           <el-form-item style="text-align: center">
             <el-button type="primary" @click="submitForm">保存</el-button>
           </el-form-item>
@@ -93,6 +41,7 @@
 </template>
 <script type="es6">
   import {mapGetters, mapActions} from 'vuex'
+  import {InlineCalendar} from 'vux'
   import filter from '../../../filters'
   import MyUpload from '../../../components/public/MyUpload.vue'
   export default {
@@ -119,7 +68,7 @@
       }
     },
     components: {
-      MyUpload
+      MyUpload, InlineCalendar
     },
     computed: {
       ...mapGetters(['data']),
