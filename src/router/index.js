44be3df7e44468b7ce6b1ed932eb14d47d7b1e03
weizidong <weizidong@ichuangye.cn>
2017-04-24 21:20:45 +0800

    title修正

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import routes from "./routers"
+import {CREATE} from '../constant'
 Vue.use(Router)
 const router = new Router({
   mode: 'history',
   routes
 })
-router.afterEach(route => route.meta ? document.title = route.meta : '')
+router.afterEach(route => {
+  if (route.meta && route.params.id == CREATE) {
+    document.title = "添加" + route.meta.substr(2)
+  } else if (route.meta) {
+    document.title = route.meta
+  }
+})
 export default router
