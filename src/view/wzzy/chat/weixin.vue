e0013944c13d6103f346e463997b63ec4f5f501f
weizidong <weizidong@ichuangye.cn>
2017-05-05 14:21:32 +0800

    微信二维码

--- src/view/wzzy/chat/weixin.vue
+++ src/view/wzzy/chat/weixin.vue
@@ -1,48 +1,18 @@
 <style lang="less">
-    #codeBox{
-        text-align: center;padding: 100px 0;
-        p{ color: #666666;font-size: 16px;}
-        img{ }
-    }
+  #codeBox {text-align: center;padding: 100px 0;
+    p {color: #666666;font-size: 16px;}
+  }
 </style>
 <template>
-    <div class="codeBox" id="codeBox">
-        <p>扫码关注微信公众号</p>
-        <img src="../../../../static/wzzy/code.jpg">
-    </div>
+  <div class="codeBox" id="codeBox">
+    <p>扫码关注微信公众号</p>
+    <img :src="setting.wechatTicket">
+  </div>
 </template>
 
 <script type="es6">
-    import { mapGetters } from 'vuex'
-    import { mapActions } from 'vuex'
-    import filters from '../../../filters'
-    import tip from '../components/tips.vue'
-    import someIcon from '../components/someIcon.vue'
-    import lastDynamic from '../components/lastDynamic.vue'
-    import question from '../components/question.vue'
-    export default{
-        data(){
-            return{
-            }
-        },
-        components:{
-            tip,someIcon,lastDynamic,question
-        },
-        computed: {
-            ...mapGetters([ 'page','list']),
-            active(){
-                return this.$route.path.replace('/view/wzzy/','');
-            }
-        },
-        methods:{
-            ...mapActions(['go','clear','getMine','changePage']),
-            ...filters,
-        },
-        created () {
-
-        },
-        destroyed(){
-
-        }
-    }
+  import {mapGetters} from 'vuex'
+  export default{
+    computed: {...mapGetters(['setting']),},
+  }
 </script>
